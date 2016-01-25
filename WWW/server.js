2841df18fe69e61e453b6f22aa6ca49509ef49d7
Tom Maxwell <tom.maxwell@truedigital.co.uk>
2016-01-25 14:22:35 +0000

    fix start script

--- WWW/server.js
+++ WWW/server.js
@@ -1,8 +1,18 @@
 var server=require('node-http-server');
 
+server.onRequest=onRequest;
+
+
+function onRequest(request){
+//console.log(request);
+}
+
+
 server.deploy(
 	{
-		port:5005,
-		root:'./public'
+		port:5000,
+		root:'~/www/public/',
+		log         : true
+        //logFunction : serverLogging,
 	}
 );
